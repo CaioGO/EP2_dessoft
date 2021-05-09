e3fd25c0d922f5e2ce6d4f1fbe0b3a0f5603a3e1
CaioGO <caio54777@gmail.com>
2021-05-09 20:00:53 -0300

    possiveis corr

--- Funções/Possiveis.py
+++ Funções/Possiveis.py
@@ -1,3 +1,41 @@
+# Função usada 1
+def extrai_valor(x):
+    c = x[0 : len(x) - 1]
+    return c
+
+# Função usada 2
+def extrai_naipe(x):
+    j = x[-1]
+    return j
+
+# Função usada 3
+def lista_movimentos_possiveis(x, y):
+    dev = []
+    i = 1
+
+    while i <= 4:
+        if x[y - i] == x[y]:
+            dev.append(i)
+            i += 2
+        elif y == 0:
+            break
+
+        elif x[y - i] != x[y]:
+            if y - i < 0:
+                break
+            elif extrai_valor(x[y - i]) == extrai_valor(x[y]):
+                dev.append(i)
+                i += 2
+            elif extrai_naipe(x[y - i]) == extrai_naipe(x[y]):
+                dev.append(i)
+                i += 2
+            else:
+                i += 2
+        else:
+            i += 2
+
+    return dev
+
 def possui_movimentos_possiveis(bar):
 
     for e in range(len(bar)):     
