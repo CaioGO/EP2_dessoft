1cc14495972934d2b71bac6af712d33b662c3bb6
CaioGO <caio54777@gmail.com>
2021-05-10 15:20:34 -0300

    game3 pt final

--- Game/game3.py
+++ Game/game3.py
@@ -9,3 +9,13 @@ def g3(bar):
         novo_jogo = str(input("Quer jogar de novo? (s/n): "))
 
         return novo_jogo
+
+    else:
+
+        print("YOU WIN!")
+
+        print("------------")
+
+        novo_jogo = str(input("Quer de jogar de novo? (s/n): "))
+
+        return novo_jogo
